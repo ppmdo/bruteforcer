7b77854030bd34ed75ba173d8552d5bad0cdc662
Pablo Prieto <pabloprieto@live.com>
2020-02-15 14:44:25 +0100

    Initial numpy commit

---
+++ src/brute.py
@@ -0,0 +1,44 @@
+from itertools import combinations, islice
+from scipy.special import comb as ncr
+import builtins
+import numpy as np
+import numba
+
+try:
+    builtins.profile
+except AttributeError:
+    # No line profiler, provide a pass-through version
+    def profile(func): return func
+    builtins.profile = profile
+
+
+@profile
+def bruteforce(data: list, target: int, max_elems: int) -> list:
+    winners = []
+
+    # Filter down data to remove zeros
+    data_len = len(data)
+    data = list(filter(lambda x: x[1] != 0, data))
+
+    combs_groups: list = []
+    max_combs: int = 0
+    for x in range(1, max_elems+1):
+        combs_groups.append(combinations(data, x))
+        max_combs += ncr(data_len, x)
+
+    for group in combs_groups:
+        while True:
+            batch = list(islice(group, 5000))
+            batch_results = np.zeros(len(batch))
+
+            if len(batch) == 0:
+                break
+
+            for i, combination in enumerate(batch):
+                batch_results[i] = abs(sum(x[1] for x in combination) - abs(target))
+
+            indexes = np.where(batch_results <= 10)[0]
+            for x in indexes:
+                winners.append((batch[x], batch_results[x]))
+
+    return list(filter(lambda x: len(x) > 0, winners))
